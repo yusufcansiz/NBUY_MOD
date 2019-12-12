a1ac3125798f9109e5c8fd01750691cec1e7582c
yusuf cansız <cansiz_yusuf@hotmail.com>
2019-12-12 19:58:56 +0300

    asd

---
+++ NBUY_MOD.Entities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBUY_MOD.Entities
+{
+    public class Category
+    {
+        public Category()
+        {
+            News = new List<News>();
+        }
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        
+        public IEnumerable<News> News { get; set; }
+    }
+
+    
+}
---
+++ NBUY_MOD.Entities/News.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBUY_MOD.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string NewsName { get; set; }
+
+        public string NewsDescription { get; set; }
+
+        //NewsDetails
+        public string NewsTitle { get; set; }
+
+        public string NewsSubtitle { get; set; }
+
+        public string NewsContent { get; set; }
+
+        public string NewsImagePath { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
